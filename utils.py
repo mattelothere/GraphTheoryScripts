14b4fe3ae9434bffcfa720bb10c063d158e631d3
mattelothere <matteo.launay@gmail.com>
2025-02-19 15:23:11 +0100

    initial commit

---
+++ utils.py
@@ -0,0 +1,11 @@
+def read_graph_from_file(path_to_file:str):
+    """ reads graph from a file and returns its data """
+    pass
+
+
+if __name__ == "__main__":
+    print("utils.py tests :")
+
+
+
+    

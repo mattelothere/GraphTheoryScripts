2aa7328b4fb83271385a594b5cb93ffc5231afa3
mattelothere <matteo.launay@gmail.com>
2025-02-21 15:25:02 +0100

    Continued method stubs

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
+from graph import Graph
+
+
 def read_graph_from_file(path_to_file:str):
     """ reads graph from a file and returns its data """
     pass
+
+
+def save_graph_to_file(graph_data:Graph):
+    pass
+
+
 
 
 if __name__ == "__main__":
